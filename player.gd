71f09f9089042c7cbfd08702b6315bfa1c39f36f
kyle6193 <kyle.transue@gmail.com>
2025-07-30 13:28:44 -0400

    fix impulse process

--- player.gd
+++ player.gd
@@ -2,7 +2,6 @@ extends RigidBody2D
 
 var impulse_force : int = 1200
 
-func _process(delta):
+func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
-		apply_central_force(Vector2.UP * impulse_force)
-		
+		apply_central_impulse(Vector2.UP * impulse_force)
